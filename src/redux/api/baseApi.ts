dc4e9b6af0cbed885169628c7f70a9362c46641b
Mohammad Ibrahim <mdiibrahim549@gmail.com>
2024-10-29 13:34:59 +0600

    pie chart added in dashboard

--- src/redux/api/baseApi.ts
+++ src/redux/api/baseApi.ts
@@ -4,7 +4,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: `http://localhost:5000/api`,
+    baseUrl: `https://posh-foods-server.vercel.app/api`,
     prepareHeaders: (headers, { getState }) => {
       const token = (getState() as any).auth.token;
 
